36289507f6194b56edd74dc0f8e21d832a1a7a42
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-11-20 20:08:38 -0500

    added login form + conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 
 const App = () => {
-  const [notes, setNotes] = useState([]); // notes state
-  const [newNote, setNewNote] = useState(""); // newNote state
-  const [showAll, setShowAll] = useState(true); // showAll state
-  const [errorMessage, setErrorMessage] = useState(null); // errorMessage state
+  const [notes, setNotes] = useState([]); // state for the entire list of notes
+  const [newNote, setNewNote] = useState(""); // state for the current note being entered in the text field
+  const [showAll, setShowAll] = useState(true); // state for showing all notes or only important notes
+  const [errorMessage, setErrorMessage] = useState(null); // state for displaying error messages
+  const [username, setUsername] = useState(""); // state for the username
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   // useEffect hook is used to fetch all notes from the server when the component is rendered
   useEffect(() => {
@@ -54,10 +58,60 @@ const App = () => {
       });
   };
 
-  // handleNoteChange function is used to update the newNote state
+  // handleNoteChange function updates newNote state to current input in text field
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
@@ -65,6 +119,14 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
